cf17f1de3c87ba49ff3f01aba029f4645ed5b45f
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-07-10 10:25:53 +0300

    chore: add taiga-ui

--- apps/taiga-lumbermill/src/app/app.component.ts
+++ apps/taiga-lumbermill/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, TuiRoot],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
--- apps/taiga-lumbermill/src/app/app.config.ts
+++ apps/taiga-lumbermill/src/app/app.config.ts
@@ -1,10 +1,14 @@
+import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+        provideAnimations(),
+        provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-  ],
+        NG_EVENT_PLUGINS
+    ],
 };
