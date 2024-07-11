32c3fd12ac39b076d89d7d919c897125e04e0f93
MishaZhem <mz-100@mail.ru>
2024-07-11 19:10:50 +0300

    chore: Add navigation

--- apps/taiga-lumbermill/src/main.ts
+++ apps/taiga-lumbermill/src/main.ts
@@ -2,7 +2,7 @@
 
 import {bootstrapApplication} from '@angular/platform-browser';
 
-import {AppComponent} from './app/app.component';
-import {appConfig} from './app/app.config';
+import {AppComponent} from './components/app/app.component';
+import {appConfig} from './components/app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err: any) => console.error(err));
