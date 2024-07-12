53cc31b0202b08ba115dc439e6b5ae7f26d6bc93
MishaZhem <mz-100@mail.ru>
2024-07-12 18:19:33 +0300

    chore: Add Base Line Chart

--- apps/taiga-lumbermill/src/dashboards/iot/iot.component.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/iot.component.ts
@@ -1,10 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiTiles} from '@taiga-ui/kit';
+
+import {LineChartComponent} from './components/LineChart/line-chart.component';
 
 @Component({
     standalone: true,
     selector: 'app-iot',
-    imports: [CommonModule],
+    imports: [CommonModule, LineChartComponent, TuiTiles],
     templateUrl: './iot.component.html',
     styleUrl: './iot.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
