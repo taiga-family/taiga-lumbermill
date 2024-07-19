d3233c9ebf75724c8e11d6c5f2bc34ba91ee90cd
MishaZhem <mz-100@mail.ru>
2024-07-19 10:18:15 +0300

    change: electricity service

--- apps/taiga-lumbermill/src/dashboards/iot/components/electricity/electricity.component.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/components/electricity/electricity.component.ts
@@ -7,10 +7,14 @@ import {
     TuiLineDaysChart,
     TuiLineDaysChartHint,
 } from '@taiga-ui/addon-charts';
-import {TuiFilterPipe, TuiMapperPipe} from '@taiga-ui/cdk';
-import {TuiAppearance} from '@taiga-ui/core';
+import type {TuiDayLike, TuiMapper, TuiMatcher} from '@taiga-ui/cdk';
+import {TuiDay, TuiDayRange, TuiFilterPipe, TuiMapperPipe, TuiMonth} from '@taiga-ui/cdk';
+import type {TuiPoint} from '@taiga-ui/core';
+import {TUI_MONTHS, TuiAppearance} from '@taiga-ui/core';
 import {TuiCardLarge} from '@taiga-ui/layout';
 import {TuiInputDateRangeModule} from '@taiga-ui/legacy';
+import type {Observable} from 'rxjs';
+import {map, of} from 'rxjs';
 
 import {ElectricityService} from './electricity.service';
 
@@ -39,4 +43,102 @@ import {ElectricityService} from './electricity.service';
 })
 export class ElectricityComponent {
     protected electricityService = inject(ElectricityService);
+
+    protected readonly months$ = inject(TUI_MONTHS);
+
+    protected show = new TuiDayRange(
+        TuiDay.currentLocal(),
+        TuiDay.currentLocal().append({month: 3}),
+    );
+
+    protected days = this.electricityService.random(this.show);
+
+    protected readonly maxLength: TuiDayLike = {month: 4};
+
+    protected get range(): TuiDayRange {
+        return this.computeRange(this.show);
+    }
+
+    protected getWidth({from, to}: TuiDayRange): number {
+        return TuiDay.lengthBetween(from, to);
+    }
+
+    protected getDate(day: TuiDay | number, date: TuiDay): TuiDay {
+        return day instanceof TuiDay ? day : date.append({day});
+    }
+
+    protected labels({from, to}: TuiDayRange): Observable<readonly string[]> {
+        const length = TuiDay.lengthBetween(from, to);
+
+        if (length > 90) {
+            return this.months$.pipe(
+                map((months: any) =>
+                    Array.from(
+                        {length: TuiMonth.lengthBetween(from, to) + 1},
+                        (_, i) => months[from.append({month: i}).month],
+                    ),
+                ),
+            );
+        }
+
+        const range = Array.from({length}, (_, day) => from.append({day}));
+        const mondays = this.electricityService.onlyMondays(range);
+        const days = range.map(String);
+
+        if (length > 60) {
+            return of(this.electricityService.even(mondays));
+        }
+
+        if (length > 14) {
+            return of(mondays);
+        }
+
+        if (length > 7) {
+            return of(this.electricityService.even(days));
+        }
+
+        return of(days);
+    }
+
+    protected readonly filter: TuiMatcher<[readonly [TuiDay, number], TuiDayRange]> = (
+        [day],
+        {from, to},
+    ) => day.daySameOrAfter(from) && day.daySameOrBefore(to);
+
+    protected readonly toNumbers: TuiMapper<
+        [ReadonlyArray<readonly [TuiDay, number]>, TuiDayRange],
+        readonly TuiPoint[]
+    > = (days, {from}) =>
+        days.map(([day, value]) => [TuiDay.lengthBetween(from, day), value]);
+
+    protected onDataChange(data: TuiDayRange): void {
+        this.days = this.electricityService.random(data);
+    }
+
+    protected computeRange(range: TuiDayRange): TuiDayRange {
+        const {from, to} = range;
+        const length = TuiDay.lengthBetween(from, to);
+        const dayOfWeekFrom = from.dayOfWeek();
+        const dayOfWeekTo = to.dayOfWeek();
+        const mondayFrom = dayOfWeekFrom ? from.append({day: 7 - dayOfWeekFrom}) : from;
+        const mondayTo = dayOfWeekTo ? to.append({day: 7 - dayOfWeekTo}) : to;
+        const mondaysLength = TuiDay.lengthBetween(mondayFrom, mondayTo);
+
+        if (length > 90) {
+            return range;
+        }
+
+        if (length > 60) {
+            return new TuiDayRange(
+                mondayFrom,
+                mondayTo.append({day: mondaysLength % 14}),
+            );
+        }
+
+        if (length > 14) {
+            return new TuiDayRange(mondayFrom, mondayTo);
+        }
+
+        return new TuiDayRange(from, to.append({day: length % 2}));
+    }
 }
--- apps/taiga-lumbermill/src/dashboards/iot/components/electricity/electricity.service.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/components/electricity/electricity.service.ts
@@ -1,114 +1,12 @@
-import {inject, Injectable} from '@angular/core';
-import type {TuiDayLike, TuiMapper, TuiMatcher} from '@taiga-ui/cdk';
-import {TuiDay, TuiDayRange, TuiMonth} from '@taiga-ui/cdk';
-import type {TuiPoint} from '@taiga-ui/core';
-import {TUI_MONTHS} from '@taiga-ui/core';
-import type {Observable} from 'rxjs';
-import {map, of} from 'rxjs';
+import {Injectable} from '@angular/core';
+import type {TuiDayRange} from '@taiga-ui/cdk';
+import {TuiDay} from '@taiga-ui/cdk';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ElectricityService {
-    private readonly months$ = inject(TUI_MONTHS);
-
-    public show = new TuiDayRange(
-        TuiDay.currentLocal(),
-        TuiDay.currentLocal().append({month: 3}),
-    );
-
-    public days = this.random(this.show);
-
-    public readonly maxLength: TuiDayLike = {month: 4};
-
-    public get range(): TuiDayRange {
-        return this.computeRange(this.show);
-    }
-
-    public getWidth({from, to}: TuiDayRange): number {
-        return TuiDay.lengthBetween(from, to);
-    }
-
-    public getDate(day: TuiDay | number, date: TuiDay): TuiDay {
-        return day instanceof TuiDay ? day : date.append({day});
-    }
-
-    public labels({from, to}: TuiDayRange): Observable<readonly string[]> {
-        const length = TuiDay.lengthBetween(from, to);
-
-        if (length > 90) {
-            return this.months$.pipe(
-                map((months: any) =>
-                    Array.from(
-                        {length: TuiMonth.lengthBetween(from, to) + 1},
-                        (_, i) => months[from.append({month: i}).month],
-                    ),
-                ),
-            );
-        }
-
-        const range = Array.from({length}, (_, day) => from.append({day}));
-        const mondays = onlyMondays(range);
-        const days = range.map(String);
-
-        if (length > 60) {
-            return of(even(mondays));
-        }
-
-        if (length > 14) {
-            return of(mondays);
-        }
-
-        if (length > 7) {
-            return of(even(days));
-        }
-
-        return of(days);
-    }
-
-    public readonly filter: TuiMatcher<[readonly [TuiDay, number], TuiDayRange]> = (
-        [day],
-        {from, to},
-    ) => day.daySameOrAfter(from) && day.daySameOrBefore(to);
-
-    public readonly toNumbers: TuiMapper<
-        [ReadonlyArray<readonly [TuiDay, number]>, TuiDayRange],
-        readonly TuiPoint[]
-    > = (days, {from}) =>
-        days.map(([day, value]) => [TuiDay.lengthBetween(from, day), value]);
-
-    public onDataChange(data: TuiDayRange): void {
-        this.days = this.random(data);
-    }
-
-    private computeRange(range: TuiDayRange): TuiDayRange {
-        const {from, to} = range;
-        const length = TuiDay.lengthBetween(from, to);
-        const dayOfWeekFrom = from.dayOfWeek();
-        const dayOfWeekTo = to.dayOfWeek();
-        const mondayFrom = dayOfWeekFrom ? from.append({day: 7 - dayOfWeekFrom}) : from;
-        const mondayTo = dayOfWeekTo ? to.append({day: 7 - dayOfWeekTo}) : to;
-        const mondaysLength = TuiDay.lengthBetween(mondayFrom, mondayTo);
-
-        if (length > 90) {
-            return range;
-        }
-
-        if (length > 60) {
-            return new TuiDayRange(
-                mondayFrom,
-                mondayTo.append({day: mondaysLength % 14}),
-            );
-        }
-
-        if (length > 14) {
-            return new TuiDayRange(mondayFrom, mondayTo);
-        }
-
-        return new TuiDayRange(from, to.append({day: length % 2}));
-    }
-
-    private generateRandomData(
+    public generateRandomData(
         {from, to}: TuiDayRange,
         initial: number,
     ): ReadonlyArray<[TuiDay, number]> {
@@ -130,15 +28,15 @@ export class ElectricityService {
             .filter(([day]) => day.dayOfWeek() < 5);
     }
 
-    private random(data: TuiDayRange): ReadonlyArray<ReadonlyArray<[TuiDay, number]>> {
+    public random(data: TuiDayRange): ReadonlyArray<ReadonlyArray<[TuiDay, number]>> {
         return [this.generateRandomData(data, 100)];
     }
-}
 
-function onlyMondays(range: readonly TuiDay[]): readonly string[] {
-    return range.filter((day) => !day.dayOfWeek()).map(String);
-}
+    public onlyMondays(range: readonly TuiDay[]): readonly string[] {
+        return range.filter((day) => !day.dayOfWeek()).map(String);
+    }
 
-function even<T>(array: readonly T[]): readonly T[] {
-    return array.filter((_, i) => !(i % 2));
+    public even<T>(array: readonly T[]): readonly T[] {
+        return array.filter((_, i) => !(i % 2));
+    }
 }
