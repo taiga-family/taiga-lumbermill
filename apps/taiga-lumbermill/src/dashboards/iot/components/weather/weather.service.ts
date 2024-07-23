169aafc230ffcce85024ed458c8b4d11412b8bb2
MishaZhem <mz-100@mail.ru>
2024-07-23 12:13:43 +0300

    chore: add util weather

--- apps/taiga-lumbermill/src/dashboards/iot/components/weather/weather.service.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/components/weather/weather.service.ts
@@ -3,6 +3,7 @@ import {inject, Injectable} from '@angular/core';
 import type {Observable} from 'rxjs';
 
 import type {ResponseData, WeatherData} from './weather.interface';
+import {WeatherUtil} from './weather.util';
 
 export const INITIAL_DATA: WeatherData[] = [
     {day: 'Fri', temp: '21°/25°', icon: '@tui.snowflake'},
@@ -17,7 +18,11 @@ export class WeatherService {
     protected http = inject(HttpClient);
     protected readonly API_KEY = '1df6860ee44f43d693d113704242207';
     protected readonly city = 'Moscow';
+    protected weatherUtil = inject(WeatherUtil);
     public readonly weatherData = INITIAL_DATA;
+    public getWeekDay = this.weatherUtil.getWeekDay;
+    public getTypeOfWeather = this.weatherUtil.getTypeOfWeather;
+    public getImage = this.weatherUtil.getImage;
 
     public getWeather(): Observable<ResponseData> {
         return this.http.get<ResponseData>(
@@ -31,73 +36,5 @@ export class WeatherService {
                 },
             },
         );
-    }
-
-    public getWeekDay(value: string | undefined): string {
-        if (value === undefined || value === null) {
-            return '';
-        }
-
-        const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-        const valueSplit = value.split('-');
-        const date = new Date(
-            parseInt(valueSplit[0], 10),
-            parseInt(valueSplit[1], 10) - 1,
-            parseInt(valueSplit[2], 10),
-        );
-
-        return days[date.getDay()];
-    }
-
-    public getTypeOfWeather(value: number | undefined): string {
-        if (value === undefined) {
-            return '';
-        }
-
-        const clouds = [1003, 1006, 1030, 1087, 1135];
-        const rain = [
-            1009, 1063, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1204, 1240, 1243,
-            1246, 1249, 1252, 1276,
-        ];
-        const snow = [
-            1066, 1069, 1072, 1114, 1117, 1147, 1168, 1171, 1204, 1210, 1213, 1216, 1219,
-            1222, 1225, 1237, 1255, 1258, 1261, 1264, 1273, 1279, 1282,
-        ];
-
-        if (clouds.includes(value)) {
-            return 'clouds';
-        }
-
-        if (rain.includes(value)) {
-            return 'rain';
-        }
-
-        if (snow.includes(value)) {
-            return 'snow';
-        }
-
-        return 'sun';
-    }
-
-    public getImage(value: number | undefined): string {
-        if (value === undefined) {
-            return '';
-        }
-
-        const type = this.getTypeOfWeather(value);
-
-        if (type === 'clouds') {
-            return 'url(/weather/clouds.jpg)';
-        }
-
-        if (type === 'rain') {
-            return 'url(/weather/rain.jpg)';
-        }
-
-        if (type === 'snow') {
-            return 'url(/weather/snow.jpg)';
-        }
-
-        return 'url(/weather/sun.jpg)';
     }
 }
