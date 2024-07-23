a525d427cbcbc110033fedec76d040774fe7ff52
MishaZhem <mz-100@mail.ru>
2024-07-23 14:16:30 +0300

    chore: Add virtual scroll

--- apps/taiga-lumbermill/src/dashboards/iot/components/contacts/contacts.component.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/components/contacts/contacts.component.ts
@@ -1,6 +1,17 @@
+import {
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+    CdkVirtualScrollViewport,
+} from '@angular/cdk/scrolling';
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {TuiAppearance, TuiIcon, TuiTitle} from '@taiga-ui/core';
+import {
+    TuiAppearance,
+    TuiIcon,
+    TuiScrollable,
+    TuiScrollbar,
+    TuiTitle,
+} from '@taiga-ui/core';
 import {TuiHeader} from '@taiga-ui/experimental';
 import {
     TuiAvatar,
@@ -30,11 +41,17 @@ import {ContactsService} from './contacts.service';
         TuiFade,
         TuiBadge,
         TuiBadgedContent,
+        CdkVirtualScrollViewport,
+        CdkFixedSizeVirtualScroll,
+        CdkVirtualForOf,
+        TuiScrollable,
+        TuiScrollbar,
     ],
     templateUrl: './contacts.component.html',
     styleUrl: './contacts.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactsComponent {
+    protected items = Array.from({length: 10000}).map((_, i) => `Item #${i}`);
     protected contactsService = inject(ContactsService);
 }
