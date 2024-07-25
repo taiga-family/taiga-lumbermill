2c70de09f6cd7958d337ef42a9d7031daf7a73e8
MishaZhem <mz-100@mail.ru>
2024-07-25 21:02:38 +0300

    fix: fix deploy

--- apps/taiga-lumbermill/src/components/app/app.component.ts
+++ apps/taiga-lumbermill/src/components/app/app.component.ts
@@ -1,4 +1,3 @@
-import {APP_BASE_HREF} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {TuiRoot} from '@taiga-ui/core';
@@ -11,7 +10,6 @@ import {NavigationComponent} from '../navigation/navigation.component';
     imports: [NavigationComponent, RouterModule, TuiRoot],
     templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [{provide: APP_BASE_HREF, useValue: '/taiga-lumbermill/'}],
 })
 export class AppComponent {
     public title = 'taiga-lumbermill';
