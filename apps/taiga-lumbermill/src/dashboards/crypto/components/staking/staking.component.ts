4fbc388e44cfd93c220375428bf05fdf27e73251
MishaZhem <mz-100@mail.ru>
2024-07-29 20:50:50 +0300

    chore: add dialog

--- apps/taiga-lumbermill/src/dashboards/crypto/components/staking/staking.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/components/staking/staking.component.ts
@@ -1,9 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {TuiAppearance, TuiButton, TuiTitle} from '@taiga-ui/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TuiAutoFocus} from '@taiga-ui/cdk';
+import {TuiAppearance, TuiButton, TuiDialog, TuiHint, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiInputModule, TuiInputNumberModule} from '@taiga-ui/legacy';
 
+import type {PricesData} from '../../../../services/crypto.service';
 import {CryptoService} from '../../../../services/crypto.service';
 
 @Component({
@@ -11,11 +15,19 @@ import {CryptoService} from '../../../../services/crypto.service';
     selector: 'lmb-staking',
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
         TuiAppearance,
+        TuiAutoFocus,
         TuiAvatar,
         TuiButton,
+        TuiButton,
         TuiCardLarge,
+        TuiDialog,
         TuiHeader,
+        TuiHint,
+        TuiInputModule,
+        TuiInputNumberModule,
         TuiTitle,
     ],
     templateUrl: './staking.component.html',
@@ -25,4 +37,31 @@ import {CryptoService} from '../../../../services/crypto.service';
 export class StakingComponent {
     protected cryptoService = inject(CryptoService);
     protected info$ = this.cryptoService.info$;
+    protected amount = 0;
+    protected avaliable = 100;
+
+    protected openStake = false;
+    protected openUnstake = false;
+
+    protected showDialogStake(): void {
+        this.openStake = true;
+    }
+
+    protected showDialogUnstake(): void {
+        this.openUnstake = true;
+    }
+
+    protected getPrice(data: PricesData[], title: string, value: number | null): string {
+        if (value === null) {
+            return '0';
+        }
+
+        for (const token of data) {
+            if (token && token.symbol.toLowerCase() === title.toLowerCase()) {
+                return (Number(token.priceUsd) * value).toFixed(2);
+            }
+        }
+
+        return value.toFixed(2);
+    }
 }
