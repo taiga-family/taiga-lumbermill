55594f5c89dad52374c3a791ac78e65b7080afde
MishaZhem <mz-100@mail.ru>
2024-07-30 15:07:22 +0300

    chore: add pools component

--- apps/taiga-lumbermill/src/dashboards/crypto/crypto.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/crypto.component.ts
@@ -1,11 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 
+import {PoolsComponent} from './components/pools/pools.component';
+
 @Component({
     standalone: true,
     selector: 'lmb-crypto',
-    imports: [CommonModule],
+    imports: [CommonModule, PoolsComponent],
     templateUrl: './crypto.component.html',
+    styleUrl: './crypto.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CryptoComponent {}
