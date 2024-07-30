c7f1f80ac16eeeb99418926c51a16004122e68ff
MishaZhem <mz-100@mail.ru>
2024-07-30 15:27:58 +0300

    chore: add staking component

---
+++ apps/taiga-lumbermill/src/dashboards/crypto/components/staking/staking.component.ts
@@ -0,0 +1,67 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TuiAutoFocus} from '@taiga-ui/cdk';
+import {TuiAppearance, TuiButton, TuiDialog, TuiHint, TuiTitle} from '@taiga-ui/core';
+import {TuiAvatar} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiInputModule, TuiInputNumberModule} from '@taiga-ui/legacy';
+
+import type {PricesData} from '../../../../services/crypto.service';
+import {CryptoService} from '../../../../services/crypto.service';
+
+@Component({
+    standalone: true,
+    selector: 'lmb-staking',
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TuiAppearance,
+        TuiAutoFocus,
+        TuiAvatar,
+        TuiButton,
+        TuiButton,
+        TuiCardLarge,
+        TuiDialog,
+        TuiHeader,
+        TuiHint,
+        TuiInputModule,
+        TuiInputNumberModule,
+        TuiTitle,
+    ],
+    templateUrl: './staking.component.html',
+    styleUrl: './staking.component.less',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StakingComponent {
+    protected cryptoService = inject(CryptoService);
+    protected info$ = this.cryptoService.info$;
+    protected amount = 0;
+    protected available = 100;
+
+    protected openStake = false;
+    protected openUnstake = false;
+
+    protected showDialogStake(): void {
+        this.openStake = true;
+    }
+
+    protected showDialogUnstake(): void {
+        this.openUnstake = true;
+    }
+
+    protected getPrice(data: PricesData[], title: string, value: number | null): string {
+        if (value === null) {
+            return '0';
+        }
+
+        for (const token of data) {
+            if (token && token.symbol.toLowerCase() === title.toLowerCase()) {
+                return (Number(token.priceUsd) * value).toFixed(2);
+            }
+        }
+
+        return value.toFixed(2);
+    }
+}
--- apps/taiga-lumbermill/src/dashboards/crypto/crypto.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/crypto.component.ts
@@ -1,11 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 
+import {StakingComponent} from './components/staking/staking.component';
+
 @Component({
     standalone: true,
     selector: 'lmb-crypto',
-    imports: [CommonModule],
+    imports: [CommonModule, StakingComponent],
     templateUrl: './crypto.component.html',
+    styleUrl: './crypto.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CryptoComponent {}
