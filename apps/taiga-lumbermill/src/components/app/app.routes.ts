cdabf14010df77e98ce226ceb6b2c002355822b4
Michael <85116490+MishaZhem@users.noreply.github.com>
2024-07-30 12:18:05 +0300

    chore: add dashboard routing (#69)

--- apps/taiga-lumbermill/src/components/app/app.routes.ts
+++ apps/taiga-lumbermill/src/components/app/app.routes.ts
@@ -2,9 +2,16 @@ import type {Route} from '@angular/router';
 
 export const appRoutes: Route[] = [
     {
-        path: 'dashboards',
+        path: 'dashboards/iot',
         loadComponent: async () =>
             import('../../dashboards/iot/iot.component').then((mod) => mod.IotComponent),
+    },
+    {
+        path: 'dashboards/crypto',
+        loadComponent: async () =>
+            import('../../dashboards/crypto/crypto.component').then(
+                (mod) => mod.CryptoComponent,
+            ),
     },
     {path: '**', redirectTo: 'dashboards'},
 ];
