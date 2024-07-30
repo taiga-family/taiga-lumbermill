e79b3c6ab3b1ba2e210752330f2a9c94716604d4
MishaZhem <mz-100@mail.ru>
2024-07-30 17:58:59 +0300

    chore: add inouts

--- apps/taiga-lumbermill/src/dashboards/crypto/components/minter/minter.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/components/minter/minter.component.ts
@@ -1,24 +1,31 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {TuiAppearance, TuiIcon, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiCell, TuiHeader} from '@taiga-ui/layout';
+import {TuiInputModule} from '@taiga-ui/legacy';
 
 @Component({
     standalone: true,
     selector: 'lmb-minter',
     imports: [
         CommonModule,
+        FormsModule,
         TuiAppearance,
         TuiAvatar,
         TuiCardLarge,
         TuiCell,
         TuiHeader,
         TuiIcon,
+        TuiInputModule,
         TuiTitle,
     ],
     templateUrl: './minter.component.html',
     styleUrl: './minter.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MinterComponent {}
+export class MinterComponent {
+    protected token = '';
+    protected symbol = '';
+}
