d62f1a22334523c3f7a08cf82a3a187ef4218c82
MishaZhem <mz-100@mail.ru>
2024-07-31 15:24:49 +0300

    feat: add interactive

--- apps/taiga-lumbermill/src/dashboards/iot/components/control-panel/control-panel.service.ts
+++ apps/taiga-lumbermill/src/dashboards/iot/components/control-panel/control-panel.service.ts
@@ -3,14 +3,40 @@ import {Injectable} from '@angular/core';
 interface ControlPanelData {
     readonly name: string;
     readonly img: string;
+    readonly clickedName: string;
+    readonly clickedImg: string;
+    state: boolean;
 }
 
 export const INITIAL_DATA: ControlPanelData[] = [
-    {name: 'Open the door', img: '@tui.door-open'},
-    {name: 'Play music', img: '@tui.speaker'},
-    {name: 'Call security', img: '@tui.shield-alert'},
-    {name: 'Turn up the heat', img: '@tui.heater'},
-    {name: 'Turn on the kettle', img: '@tui.coffee'},
+    {
+        name: 'Open the door',
+        img: '@tui.door-open',
+        clickedName: 'Close the door',
+        clickedImg: '@tui.door-closed',
+        state: false,
+    },
+    {
+        name: 'Call security',
+        img: '@tui.shield-alert',
+        clickedName: 'Сall off security',
+        clickedImg: '@tui.octagon-x',
+        state: false,
+    },
+    {
+        name: 'Turn up the heat',
+        img: '@tui.heater',
+        clickedName: 'Turn off the heat',
+        clickedImg: '@tui.refrigerator',
+        state: false,
+    },
+    {
+        name: 'Turn on the kettle',
+        img: '@tui.coffee',
+        clickedName: 'Turn off the kettle',
+        clickedImg: '@tui.cup-soda',
+        state: false,
+    },
 ];
 
 @Injectable({
