f25cecd12554126ca13ce5a11cf1eae6bbd150a2
MishaZhem <mz-100@mail.ru>
2024-08-01 09:00:04 +0300

    chore: add card medium

--- apps/taiga-lumbermill/src/dashboards/crypto/components/nft/nft.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/components/nft/nft.component.ts
@@ -1,12 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
-import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiAvatar} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiCardMedium, TuiHeader} from '@taiga-ui/layout';
 
 @Component({
     standalone: true,
     selector: 'lmb-nft',
-    imports: [CommonModule, TuiAppearance, TuiCardLarge, TuiHeader, TuiTitle],
+    imports: [
+        CommonModule,
+        TuiAppearance,
+        TuiAvatar,
+        TuiCardLarge,
+        TuiCardMedium,
+        TuiHeader,
+        TuiTitle,
+    ],
     templateUrl: './nft.component.html',
     styleUrl: './nft.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
