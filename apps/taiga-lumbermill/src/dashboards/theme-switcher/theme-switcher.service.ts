f148b87fe8b1e141936abf4e2ccd5744aa255856
MishaZhem <mz-100@mail.ru>
2024-08-06 14:06:34 +0300

    chore: add properties

--- apps/taiga-lumbermill/src/dashboards/theme-switcher/theme-switcher.service.ts
+++ apps/taiga-lumbermill/src/dashboards/theme-switcher/theme-switcher.service.ts
@@ -3,14 +3,27 @@ import {Injectable} from '@angular/core';
 interface ThemeSwitcherData {
     readonly name: string;
     readonly base_color: string;
+    readonly property: string;
 }
 
 export const INITIAL_DATA: ThemeSwitcherData[] = [
-    {name: 'Base background', base_color: 'rgba(255, 255, 255)'},
-    {name: 'Primary text', base_color: 'rgba(27, 31, 59, 1)'},
-    {name: 'Secondary', base_color: 'rgba(27, 31, 59, 0.65)'},
-    {name: 'Action', base_color: '#526ed3'},
-    {name: 'Accent', base_color: '#ff7043'},
+    {
+        name: 'Base background',
+        base_color: 'rgba(255, 255, 255)',
+        property: '--tui-background-base',
+    },
+    {
+        name: 'Primary text',
+        base_color: 'rgba(27, 31, 59, 1)',
+        property: '--tui-text-primary',
+    },
+    {
+        name: 'Secondary',
+        base_color: 'rgba(27, 31, 59, 0.65)',
+        property: '--tui-text-secondary',
+    },
+    {name: 'Action', base_color: '#526ed3', property: '--tui-text-action'},
+    {name: 'Accent', base_color: '#ff7043', property: '--tui-background-accent-2'},
 ];
 
 @Injectable({
