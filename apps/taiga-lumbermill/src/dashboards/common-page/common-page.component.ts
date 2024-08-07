406b1d0b8bd2e2d51510c1926c43dd4e7324a09b
MishaZhem <mz-100@mail.ru>
2024-08-07 16:18:42 +0300

    chore: add common-page

---
+++ apps/taiga-lumbermill/src/dashboards/common-page/common-page.component.ts
@@ -0,0 +1,14 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+
+@Component({
+    standalone: true,
+    selector: 'lmb-common-page',
+    imports: [CommonModule, TuiAppearance, TuiCardLarge, TuiHeader, TuiTitle],
+    templateUrl: './common-page.component.html',
+    styleUrl: './common-page.component.less',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommonPageComponent {}
