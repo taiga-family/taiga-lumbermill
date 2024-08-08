c814f96abe763794c7eab6e6dde59279729d2a03
MishaZhem <mz-100@mail.ru>
2024-08-08 16:46:30 +0300

    chore: add carousel

--- apps/taiga-lumbermill/src/dashboards/films/components/list-films/list-films.component.ts
+++ apps/taiga-lumbermill/src/dashboards/films/components/list-films/list-films.component.ts
@@ -1,11 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiCarousel} from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
     selector: 'lmb-list-films',
-    imports: [CommonModule],
+    imports: [CommonModule, TuiCarousel],
     templateUrl: './list-films.component.html',
+    styleUrl: './list-films.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListFilmsComponent {}
+export class ListFilmsComponent {
+    protected index = 0;
+    protected readonly items = [
+        './example.png',
+        './example.png',
+        './example.png',
+        './example.png',
+    ];
+}
--- apps/taiga-lumbermill/src/dashboards/films/films.component.ts
+++ apps/taiga-lumbermill/src/dashboards/films/films.component.ts
@@ -1,12 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiAppearance} from '@taiga-ui/core';
+import {TuiCardLarge} from '@taiga-ui/layout';
 
 import {ListFilmsComponent} from './components/list-films/list-films.component';
 
 @Component({
     standalone: true,
     selector: 'lmb-film',
-    imports: [CommonModule, ListFilmsComponent],
+    imports: [CommonModule, ListFilmsComponent, TuiAppearance, TuiCardLarge],
     templateUrl: './films.component.html',
     styleUrl: './films.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
