c449234453bb23984d0ff5eb89eeb212fa330950
MishaZhem <mz-100@mail.ru>
2024-08-09 11:25:08 +0300

    chore: remove extra code

--- apps/taiga-lumbermill/src/dashboards/common-page/common-page.component.ts
+++ apps/taiga-lumbermill/src/dashboards/common-page/common-page.component.ts
@@ -1,33 +1,15 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
-import {TuiAppearance, TuiSurface, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
-import {TUI_DEFAULT_INPUT_COLORS, TuiInputColorModule} from '@taiga-ui/legacy';
-
-import {INITIAL_DATA} from '../dashboards-list/dashboards-list.component';
 
 @Component({
     standalone: true,
     selector: 'lmb-common-page',
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterLink,
-        TuiAppearance,
-        TuiCardLarge,
-        TuiHeader,
-        TuiInputColorModule,
-        TuiSurface,
-        TuiTitle,
-    ],
+    imports: [CommonModule, RouterLink, TuiAppearance, TuiCardLarge, TuiHeader, TuiTitle],
     templateUrl: './common-page.component.html',
     styleUrl: './common-page.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommonPageComponent {
-    protected readonly dashboardsListData = INITIAL_DATA;
-    protected readonly exampleColor = '#fff';
-    protected readonly palette = TUI_DEFAULT_INPUT_COLORS;
-}
+export class CommonPageComponent {}
