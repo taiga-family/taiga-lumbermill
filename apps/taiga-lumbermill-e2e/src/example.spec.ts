4ddaf10abbe577692c1608f80b622dfd6150f83b
MishaZhem <mz-100@mail.ru>
2024-08-09 11:43:05 +0300

    fix: fix tests

--- apps/taiga-lumbermill-e2e/src/example.spec.ts
+++ apps/taiga-lumbermill-e2e/src/example.spec.ts
@@ -4,5 +4,5 @@ test('has title', async ({page}) => {
     await page.goto('/dashboards/iot');
 
     // Expect h2 to contain a substring.
-    expect(await page.locator('h2').first().innerText()).toContain('Cleaning schedule');
+    expect(await page.locator('h2').first().innerText()).toContain('Open the door');
 });
