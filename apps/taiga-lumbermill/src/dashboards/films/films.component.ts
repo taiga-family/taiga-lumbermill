bdc0fbc25cb1346c5440120d503e0c7c7c09feb5
MishaZhem <mz-100@mail.ru>
2024-08-09 15:52:42 +0300

    chore: add player

--- apps/taiga-lumbermill/src/dashboards/films/films.component.ts
+++ apps/taiga-lumbermill/src/dashboards/films/films.component.ts
@@ -3,12 +3,19 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TuiAppearance} from '@taiga-ui/core';
 import {TuiCardLarge} from '@taiga-ui/layout';
 
+import FilmViewComponent from './components/film-view/film-view.component';
 import {ListFilmsComponent} from './components/list-films/list-films.component';
 
 @Component({
     standalone: true,
     selector: 'lmb-film',
-    imports: [CommonModule, ListFilmsComponent, TuiAppearance, TuiCardLarge],
+    imports: [
+        CommonModule,
+        FilmViewComponent,
+        ListFilmsComponent,
+        TuiAppearance,
+        TuiCardLarge,
+    ],
     templateUrl: './films.component.html',
     styleUrl: './films.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
