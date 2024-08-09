32c648a162a84d2d86d9a5a75cfd2e42fb5da0b1
MishaZhem <mz-100@mail.ru>
2024-08-09 13:31:00 +0300

    chore: change table

--- apps/taiga-lumbermill/src/dashboards/crypto/components/pools/pools.component.ts
+++ apps/taiga-lumbermill/src/dashboards/crypto/components/pools/pools.component.ts
@@ -4,7 +4,7 @@ import {
     CdkVirtualScrollViewport,
 } from '@angular/cdk/scrolling';
 import {CommonModule} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {FormsModule} from '@angular/forms';
 import type {TuiComparator} from '@taiga-ui/addon-table';
@@ -44,6 +44,12 @@ import {CryptoService} from '../../../../services/crypto.service';
 export class PoolsComponent {
     protected cryptoService = inject(CryptoService);
     protected tokens = toSignal(this.cryptoService.getTokens());
+    protected tableData = computed(() =>
+        (this.tokens() ?? [])
+            .map((item, index) => [item, (this.tokens() ?? [])[index + 1]])
+            .filter((_, index) => index % 2 === 0),
+    );
+
     protected columns = ['Pair', 'TVL', 'APR'];
     protected search = '';
 
@@ -53,8 +59,8 @@ export class PoolsComponent {
 
     protected getTVL(index: number): string {
         const result =
-            Number(this.tokens()?.[index]?.priceUsd) +
-            Number(this.tokens()?.[index]?.priceUsd) +
+            Number(this.tableData()[index][0].priceUsd) +
+            Number(this.tableData()[index][1].priceUsd) +
             1;
 
         if (result > 100) {
@@ -69,5 +75,5 @@ export class PoolsComponent {
     }
 
     protected readonly aprSorter: TuiComparator<string> = (a: string, b: string) =>
-        Number(a) - Number(b);
+        Number(a.replace('%', '')) - Number(b.replace('%', ''));
 }
