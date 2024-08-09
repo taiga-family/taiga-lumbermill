a807341bd436324b0fec6c107fd24019a02f56f0
MishaZhem <mz-100@mail.ru>
2024-08-09 17:14:42 +0300

    chore: add title

--- apps/taiga-lumbermill/src/dashboards/films/components/film-view/film-view.component.ts
+++ apps/taiga-lumbermill/src/dashboards/films/components/film-view/film-view.component.ts
@@ -1,15 +1,32 @@
-import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    inject,
+    Input,
+    Output,
+} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {TuiMedia} from '@taiga-ui/cdk';
-import {TuiButton} from '@taiga-ui/core';
+import {TuiAppearance, TuiButton, TuiIcon, TuiTitle} from '@taiga-ui/core';
 import {TuiSlider} from '@taiga-ui/kit';
+import {TuiHeader} from '@taiga-ui/layout';
 
 import {FilmsService} from '../../films.service';
 
 @Component({
     standalone: true,
     selector: 'lmb-film-view',
-    imports: [FormsModule, TuiButton, TuiMedia, TuiSlider],
+    imports: [
+        FormsModule,
+        TuiAppearance,
+        TuiButton,
+        TuiHeader,
+        TuiIcon,
+        TuiMedia,
+        TuiSlider,
+        TuiTitle,
+    ],
     templateUrl: './film-view.component.html',
     styleUrls: ['./film-view.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,4 +39,11 @@ export default class FilmViewComponent {
 
     @Input()
     public play = 0;
+
+    @Output()
+    public readonly playChange = new EventEmitter<number>();
+
+    protected close(): void {
+        this.playChange.emit(-1);
+    }
 }
