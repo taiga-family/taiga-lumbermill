ce752f46eab3a863c27c8d64c0de47c323c0be78
MishaZhem <mz-100@mail.ru>
2024-08-19 15:31:14 +0300

    chore: add process circle

--- apps/taiga-lumbermill/src/dashboards/space-zoo/components/animal/animal.component.ts
+++ apps/taiga-lumbermill/src/dashboards/space-zoo/components/animal/animal.component.ts
@@ -3,15 +3,25 @@ import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {ActivatedRoute} from '@angular/router';
 import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
-import {TuiAvatar, TuiBadge} from '@taiga-ui/kit';
-import {TuiHeader} from '@taiga-ui/layout';
+import {TuiAvatar, TuiBadge, TuiProgress} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {map, startWith, takeWhile, timer} from 'rxjs';
 
 import {animals} from '../administrator/administrator.component';
 
 @Component({
     standalone: true,
     selector: 'lmb-animal-page',
-    imports: [CommonModule, TuiAppearance, TuiAvatar, TuiBadge, TuiHeader, TuiTitle],
+    imports: [
+        CommonModule,
+        TuiAppearance,
+        TuiAvatar,
+        TuiBadge,
+        TuiCardLarge,
+        TuiHeader,
+        TuiProgress,
+        TuiTitle,
+    ],
     templateUrl: './animal.component.html',
     styleUrl: './animal.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,4 +30,10 @@ export class AnimalComponent {
     private readonly activatedRoute = inject(ActivatedRoute);
     protected id = toSignal(this.activatedRoute.params)()?.['id'];
     protected animals = animals;
+    protected readonly max = 100;
+    protected readonly value$ = timer(300, 200).pipe(
+        map((i) => i + 30),
+        startWith(30),
+        takeWhile((value) => value <= this.max),
+    );
 }
