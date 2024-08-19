2aa858f65276433f847f91d38d0efecea30486d3
MishaZhem <mz-100@mail.ru>
2024-08-19 17:16:55 +0300

    chore: add copy button

--- apps/taiga-lumbermill/src/dashboards/theme-generator/theme-generator.component.ts
+++ apps/taiga-lumbermill/src/dashboards/theme-generator/theme-generator.component.ts
@@ -1,7 +1,14 @@
+import {Clipboard} from '@angular/cdk/clipboard';
 import {CommonModule} from '@angular/common';
-import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    computed,
+    inject,
+    signal,
+} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiIcon, TuiTitle} from '@taiga-ui/core';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 import {TUI_DEFAULT_INPUT_COLORS, TuiInputColorModule} from '@taiga-ui/legacy';
 
@@ -18,6 +25,7 @@ import {data} from './theme-generator.constants';
         TuiAppearance,
         TuiCardLarge,
         TuiHeader,
+        TuiIcon,
         TuiInputColorModule,
         TuiTitle,
     ],
@@ -26,11 +34,28 @@ import {data} from './theme-generator.constants';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeGeneratorComponent {
+    private readonly clipboard = inject(Clipboard);
     protected themeData = data;
     protected readonly palette = TUI_DEFAULT_INPUT_COLORS;
     protected colors = this.themeData.map((val) => signal(val.initialValue));
+    protected copied = signal(false);
 
     protected theme = computed(() =>
         this.colors.map((val, i) => `${this.themeData[i].variable}: ${val()};`).join(' '),
     );
+
+    protected copy(): void {
+        this.copied.set(true);
+        setTimeout(() => {
+            this.copied.set(false);
+        }, 800);
+        let text = ':root {';
+
+        for (let i = 0; i < this.themeData.length; i++) {
+            text += `\n${this.themeData[i].variable}: ${this.colors[i]()};`;
+        }
+
+        text += '\n}';
+        this.clipboard.copy(text);
+    }
 }
