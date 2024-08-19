f465c6c401c50aab80e578bb4c49fd3c0814bd16
MishaZhem <mz-100@mail.ru>
2024-08-19 15:54:25 +0300

    chore: change status after process

--- apps/taiga-lumbermill/src/dashboards/space-zoo/components/animal/animal.component.ts
+++ apps/taiga-lumbermill/src/dashboards/space-zoo/components/animal/animal.component.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {ActivatedRoute} from '@angular/router';
-import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiSurface, TuiTitle} from '@taiga-ui/core';
 import {TuiAvatar, TuiBadge, TuiProgress} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 import {map, startWith, takeWhile, timer} from 'rxjs';
@@ -20,6 +20,7 @@ import {animals} from '../administrator/administrator.component';
         TuiCardLarge,
         TuiHeader,
         TuiProgress,
+        TuiSurface,
         TuiTitle,
     ],
     templateUrl: './animal.component.html',
@@ -31,7 +32,7 @@ export class AnimalComponent {
     protected id = toSignal(this.activatedRoute.params)()?.['id'];
     protected animals = animals;
     protected readonly max = 100;
-    protected readonly value$ = timer(300, 200).pipe(
+    protected readonly value$ = timer(140, 80).pipe(
         map((i) => i + 30),
         startWith(30),
         takeWhile((value) => value <= this.max),
