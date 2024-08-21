be764af4c20ab449d2d261aed331395dabbbf74f
MishaZhem <mz-100@mail.ru>
2024-08-21 12:59:30 +0300

    chore: add remeber me to form

--- apps/taiga-lumbermill/src/dashboards/login/login.component.ts
+++ apps/taiga-lumbermill/src/dashboards/login/login.component.ts
@@ -47,6 +47,7 @@ export class LoginComponent {
     protected readonly form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required]),
+        rememberMe: new FormControl(false),
     });
 
     protected onSubmit(): void {}
