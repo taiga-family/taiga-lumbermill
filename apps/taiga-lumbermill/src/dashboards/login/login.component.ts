22cb68b0c5ee3b12b686e0c5739c2020fd95ef1e
MishaZhem <mz-100@mail.ru>
2024-08-22 14:10:12 +0300

    chore: update login

--- apps/taiga-lumbermill/src/dashboards/login/login.component.ts
+++ apps/taiga-lumbermill/src/dashboards/login/login.component.ts
@@ -1,5 +1,6 @@
 import {CommonModule} from '@angular/common';
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
 import {
     FormControl,
     FormGroup,
@@ -7,17 +8,20 @@ import {
     ReactiveFormsModule,
     Validators,
 } from '@angular/forms';
+import {Router} from '@angular/router';
 import {
     TuiAppearance,
     TuiButton,
     TuiError,
     TuiLabel,
     TuiLink,
+    TuiLoader,
     TuiTitle,
 } from '@taiga-ui/core';
 import {TUI_VALIDATION_ERRORS, TuiCheckbox, TuiFieldErrorPipe} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 import {TuiInputModule, TuiInputPasswordModule} from '@taiga-ui/legacy';
+import {map, startWith, Subject, switchMap, timer} from 'rxjs';
 
 @Component({
     standalone: true,
@@ -37,6 +41,7 @@ import {TuiInputModule, TuiInputPasswordModule} from '@taiga-ui/legacy';
         TuiInputPasswordModule,
         TuiLabel,
         TuiLink,
+        TuiLoader,
         TuiTitle,
     ],
     templateUrl: './login.component.html',
@@ -47,17 +52,40 @@ import {TuiInputModule, TuiInputPasswordModule} from '@taiga-ui/legacy';
             provide: TUI_VALIDATION_ERRORS,
             useValue: {
                 required: "Value can't be empty",
-                email: 'Value should be email',
+                email: 'Invalid email',
             },
         },
     ],
 })
 export class LoginComponent {
+    private readonly router = inject(Router);
     protected readonly form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', [Validators.required]),
         rememberMe: new FormControl(false),
     });
 
-    protected onSubmit(): void {}
+    protected readonly submit$ = new Subject<void>();
+
+    protected readonly submitLoader = toSignal(
+        this.submit$.pipe(
+            switchMap(() =>
+                timer(4000).pipe(
+                    map(() => this.goMain()),
+                    startWith(true),
+                ),
+            ),
+        ),
+        {initialValue: false},
+    );
+
+    protected goMain(): boolean {
+        this.router.navigate(['']);
+
+        return false;
+    }
+
+    protected onSubmit(): void {
+        this.submit$.next();
+    }
 }
