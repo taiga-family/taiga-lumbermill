28e0ade03c8291f7b0649f201baf1d29bfa9cee4
MishaZhem <mz-100@mail.ru>
2024-08-23 13:22:21 +0300

    chore: update login

--- apps/taiga-lumbermill/src/dashboards/login/login.component.ts
+++ apps/taiga-lumbermill/src/dashboards/login/login.component.ts
@@ -1,13 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {
-    FormControl,
-    FormGroup,
-    FormsModule,
-    ReactiveFormsModule,
-    Validators,
-} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {
     TuiAppearance,
@@ -21,14 +15,13 @@ import {
 import {TUI_VALIDATION_ERRORS, TuiCheckbox, TuiFieldErrorPipe} from '@taiga-ui/kit';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 import {TuiInputModule, TuiInputPasswordModule} from '@taiga-ui/legacy';
-import {map, startWith, Subject, switchMap, timer} from 'rxjs';
+import {map, of, startWith, Subject, switchMap, timer} from 'rxjs';
 
 @Component({
     standalone: true,
     selector: 'lmb-login',
     imports: [
         CommonModule,
-        FormsModule,
         ReactiveFormsModule,
         TuiAppearance,
         TuiButton,
@@ -53,6 +46,8 @@ import {map, startWith, Subject, switchMap, timer} from 'rxjs';
             useValue: {
                 required: "Value can't be empty",
                 email: 'Invalid email',
+                minlength: ({requiredLength}: {requiredLength: string}) =>
+                    of(`Minimum length — ${requiredLength}`),
             },
         },
     ],
@@ -61,7 +56,7 @@ export class LoginComponent {
     private readonly router = inject(Router);
     protected readonly form = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required, Validators.minLength(5)]),
         rememberMe: new FormControl(false),
     });
 
@@ -86,6 +81,11 @@ export class LoginComponent {
     }
 
     protected onSubmit(): void {
+        if (!this.form.valid) {
+            return;
+        }
+
+        this.form.disable();
         this.submit$.next();
     }
 }
