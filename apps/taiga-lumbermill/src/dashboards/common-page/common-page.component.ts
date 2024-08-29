09b4906cb11e8e801b8783614125d151bab80be8
MishaZhem <mz-100@mail.ru>
2024-08-29 16:56:50 +0300

    feat: update main page

--- apps/taiga-lumbermill/src/dashboards/common-page/common-page.component.ts
+++ apps/taiga-lumbermill/src/dashboards/common-page/common-page.component.ts
@@ -1,13 +1,21 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {TuiAppearance, TuiTitle} from '@taiga-ui/core';
+import {TuiAppearance, TuiSurface, TuiTitle} from '@taiga-ui/core';
 import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
 
 @Component({
     standalone: true,
     selector: 'lmb-common-page',
-    imports: [CommonModule, RouterLink, TuiAppearance, TuiCardLarge, TuiHeader, TuiTitle],
+    imports: [
+        CommonModule,
+        RouterLink,
+        TuiAppearance,
+        TuiCardLarge,
+        TuiHeader,
+        TuiSurface,
+        TuiTitle,
+    ],
     templateUrl: './common-page.component.html',
     styleUrl: './common-page.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
