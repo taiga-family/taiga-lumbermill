56008d326d12e3628fba7fc0a82b3b4b6fd8c726
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-10-07 15:40:48 +0000

    Deploying to gh-pages from @ taiga-family/taiga-lumbermill@9462e89619e4ae8d019cc874667615f97979bf4e 🚀

--- chunk-6GFOBN3Y.js
+++
@@ -1,1 +0,0 @@
-import{Ea as i}from"./chunk-Z6RZWZYC.js";var p=(()=>{let e=class e{transform(n,r,...o){return r(n,...o)}};e.\u0275fac=function(r){return new(r||e)},e.\u0275pipe=i({name:"tuiMapper",type:e,pure:!0,standalone:!0});let t=e;return t})();export{p as a};
