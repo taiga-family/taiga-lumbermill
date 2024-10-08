33eb69664111f88832a0795ccf7a638bfdfb6b38
Vladimir Potekhin <vladimir.potekh@gmail.com>
2024-10-08 17:32:38 +0300

    chore: temporary change nav

--- apps/taiga-lumbermill/src/components/app/app.routes.ts
+++ apps/taiga-lumbermill/src/components/app/app.routes.ts
@@ -4,38 +4,43 @@ export const appRoutes: Route[] = [
     {
         path: 'dashboards',
         loadComponent: async () =>
-            import('../../dashboards/dashboards/dashboards.component').then(
-                (mod) => mod.DashboardsComponent,
-            ),
-        children: [
-            {
-                path: '',
-                loadComponent: async () =>
-                    import(
-                        '../../components/dashboards-list/dashboards-list.component'
-                    ).then((mod) => mod.DashboardsListComponent),
-                data: {title: ''},
-            },
-            {
-                path: 'iot',
-                loadComponent: async () =>
-                    import('../../dashboards/iot/iot.component').then(
-                        (mod) => mod.IotComponent,
-                    ),
-                data: {title: 'Iot'},
-            },
-            {
-                path: 'crypto',
-                loadComponent: async () =>
-                    import('../../dashboards/crypto/crypto.component').then(
-                        (mod) => mod.CryptoComponent,
-                    ),
-                data: {title: 'Crypto'},
-            },
-        ],
+            import('../list/list.component').then((mod) => mod.ListComponent),
     },
     {
-        path: 'theme',
+        path: 'dashboards/iot',
+        loadComponent: async () =>
+            import('../../dashboards/iot/iot.component').then((mod) => mod.IotComponent),
+        data: {title: 'Iot'},
+    },
+    {
+        path: 'dashboards/crypto',
+        loadComponent: async () =>
+            import('../../dashboards/crypto/crypto.component').then(
+                (mod) => mod.CryptoComponent,
+            ),
+        data: {title: 'Crypto'},
+    },
+    {
+        path: 'pages',
+        loadComponent: async () =>
+            import('../list/list.component').then((mod) => mod.ListComponent),
+    },
+    {
+        path: 'pages/login',
+        loadComponent: async () =>
+            import('../../features/login/login.component').then(
+                (mod) => mod.LoginComponent,
+            ),
+    },
+    {
+        path: 'pages/sign-up',
+        loadComponent: async () =>
+            import('../../features/sign-up/sign-up.component').then(
+                (mod) => mod.SignUpComponent,
+            ),
+    },
+    {
+        path: 'color-generator',
         loadComponent: async () =>
             import('../../features/theme-generator/theme-generator.component').then(
                 (mod) => mod.ThemeGeneratorComponent,
@@ -46,20 +51,6 @@ export const appRoutes: Route[] = [
         loadComponent: async () =>
             import('../../components/common-page/common-page.component').then(
                 (mod) => mod.CommonPageComponent,
-            ),
-    },
-    {
-        path: 'login',
-        loadComponent: async () =>
-            import('../../features/login/login.component').then(
-                (mod) => mod.LoginComponent,
-            ),
-    },
-    {
-        path: 'sign-up',
-        loadComponent: async () =>
-            import('../../features/sign-up/sign-up.component').then(
-                (mod) => mod.SignUpComponent,
             ),
     },
     {path: '**', redirectTo: 'dashboards'},
---
+++ apps/taiga-lumbermill/src/components/list/list.component.ts
@@ -0,0 +1,72 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
+import {TuiIcon, TuiSurface, TuiTitle} from '@taiga-ui/core';
+import {TuiCardLarge, TuiHeader, TuiLogoComponent, TuiNavigation} from '@taiga-ui/layout';
+import {filter, map, startWith} from 'rxjs';
+
+interface CardData {
+    readonly title: string;
+    readonly link: string;
+    readonly description: string;
+}
+
+const LIST: {Dashboards: CardData[]; Pages: CardData[]} = {
+    Dashboards: [
+        {
+            title: 'Iot Dashboard',
+            link: '/dashboards/iot',
+            description: 'Smart home dashboard',
+        },
+        {
+            title: 'Crypto Dashboard',
+            link: '/dashboards/crypto',
+            description: 'Crypto token dashboard',
+        },
+    ],
+    Pages: [
+        {
+            title: 'Login',
+            link: '/pages/login',
+            description: 'Ready to use login page',
+        },
+        {
+            title: 'Sign up',
+            link: '/pages/login',
+            description: 'Ready to use registration page',
+        },
+    ],
+};
+
+@Component({
+    standalone: true,
+    selector: 'lmb-dashboards-list',
+    imports: [
+        CommonModule,
+        RouterLink,
+        RouterOutlet,
+        TuiCardLarge,
+        TuiHeader,
+        TuiIcon,
+        TuiLogoComponent,
+        TuiNavigation,
+        TuiSurface,
+        TuiTitle,
+    ],
+    templateUrl: './list.component.html',
+    styleUrl: './list.component.less',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListComponent {
+    protected readonly router = inject(Router);
+    protected readonly type = toSignal(
+        this.router.events.pipe(
+            filter((event) => event instanceof NavigationEnd),
+            startWith(null),
+            map(() => (this.router.url.includes('dashboards') ? 'Dashboards' : 'Pages')),
+        ),
+    );
+
+    protected readonly list = computed(() => LIST[this.type() ?? 'Pages']);
+}
