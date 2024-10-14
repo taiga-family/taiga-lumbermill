27d31ed639318383a3c97e89d6690fffa0e386f6
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-10-14 18:09:29 +0300

    chore: add ssr (#256)

---
+++ apps/taiga-lumbermill/src/components/app/app.config.server.ts
@@ -0,0 +1,12 @@
+import type {ApplicationConfig} from '@angular/core';
+import {mergeApplicationConfig} from '@angular/core';
+import {provideServerRendering} from '@angular/platform-server';
+import {UNIVERSAL_PROVIDERS} from '@ng-web-apis/universal';
+
+import {appConfig} from './app.config';
+
+const serverConfig: ApplicationConfig = {
+    providers: [provideServerRendering(), UNIVERSAL_PROVIDERS],
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
