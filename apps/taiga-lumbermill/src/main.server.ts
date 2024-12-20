7a3883c68b37fdfda4d7958bc29c0d3c04c82367
Vladimir Potekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-12-20 12:40:17 +0300

    chore: layout changes (#360)

--- apps/taiga-lumbermill/src/main.server.ts
+++ apps/taiga-lumbermill/src/main.server.ts
@@ -1,8 +1,8 @@
 import type {ApplicationRef} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 
-import {AppComponent} from './components/app/app.component';
-import {config} from './components/app/app.config.server';
+import {AppComponent} from './app/app.component';
+import {config} from './app/app.config.server';
 
 const bootstrap = async (): Promise<ApplicationRef> =>
     bootstrapApplication(AppComponent, config);
